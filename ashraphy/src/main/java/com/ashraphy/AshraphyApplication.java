1b32d03ecec041ff803cd5f8dc5ae9fad5a11470
Ashraphy <ashraphy9@gmail.com>
2021-12-09 18:00:29 +0530

    Hello world project creation
    Hello world project creation

---
+++ ashraphy/src/main/java/com/ashraphy/AshraphyApplication.java
@@ -0,0 +1,13 @@
+package com.ashraphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshraphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshraphyApplication.class, args);
+	}
+
+}
